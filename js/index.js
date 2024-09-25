b01f9d30bd69bfb07008df8f68b722ee49eabd4c
mdazharuddinn <mdazharuddin5832@gmail.com>
2024-09-25 08:50:09 +0600

    another part added

--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
-document.getElementById('first-donate-now').addEventListener('click',function(){
+document.getElementById('first-donate-now').addEventListener('click', function () {
     const inputFieldValue = getInputValue('input-field-money');
     const availableBalance = getInnerText('avalable-balance');
+    const availableBalanceDecrease = availableBalance - inputFieldValue;
 
-    if (inputFieldValue < 0 ||availableBalance < 0 ) {
-        alert('Not enough Money')
-        return}
+    if (availableBalanceDecrease <= 0 ||inputFieldValue<= 0) {
+        alert('Not enough Money Enter Proper Amount')
+        return
+    } else {
 
-    const textInner=getInnerText('total-donate');
-    const availableBalanceDecrease=availableBalance-inputFieldValue;
-    document.getElementById('avalable-balance').innerText=availableBalanceDecrease;
-    const sum=inputFieldValue+textInner;
-    document.getElementById('total-donate').innerText = sum;
-
+        const textInner = getInnerText('total-donate');
+        document.getElementById('avalable-balance').innerText = availableBalanceDecrease;
+        const sum = inputFieldValue + textInner;
+        document.getElementById('total-donate').innerText = sum;
+        alert('Congratulation!!! Success Your Donation'); 
+    };
 
     document.getElementById('input-field-money').value = '';
-
-    inputFieldValue.value = '';
     
-
     const historyPage = document.getElementById('history-page');
     const createElement = document.createElement('div');
     createElement.classList.add('bg-red-100');
@@ -25,13 +24,14 @@ document.getElementById('first-donate-now').addEventListener('click',function(){
     createElement.classList.add('text-center');
     createElement.classList.add('mb-5');
     createElement.classList.add('rounded');
-createElement.innerHTML = `
+    createElement.innerHTML = `
 <h3>Donate : ${inputFieldValue}</h3>
 <h3>Available Balance Now :${availableBalanceDecrease}</h3>
-<p>Date ${ new Date()}</p>
+<p>Date ${new Date()}</p>
 `
-historyPage.appendChild(createElement);
+    historyPage.appendChild(createElement);
 });
+
 
 
 document.getElementById('donation-btn').addEventListener('click', function () {
@@ -43,6 +43,6 @@ document.getElementById('donation-btn').addEventListener('click', function () {
 document.getElementById('history-btn').addEventListener('click', function () {
     getHistoryBtn('history-page');
 });
-   
+
 
 
--- js/index2.js
+++ js/index2.js
@@ -2,8 +2,44 @@ document.getElementById('second-donate-now').addEventListener('click',function()
     const inputFieldValue=getInputValue('input2-field-money');
     const textInner=getInnerText('total2-donate');
     const availableBalance=getInnerText('avalable-balance');
-    const availableBalanceDecrease=availableBalance-inputFieldValue;
+    const availableBalanceDecrease = availableBalance - inputFieldValue;
+    
+
+
+    if (availableBalanceDecrease <= 0 ||inputFieldValue<= 0) {
+        alert('Not enough Money Enter Proper Amount')
+        return
+    } else {
+
+        const textInner = getInnerText('total2-donate');
+        document.getElementById('avalable-balance').innerText = availableBalanceDecrease;
+        const sum = inputFieldValue + textInner;
+        document.getElementById('total2-donate').innerText = sum;
+        alert('Congratulation!!! Success Your Donation'); 
+    };
+
+    document.getElementById('input2-field-money').value = '';
+    
+    const historyPage = document.getElementById('history-page');
+    const createElement = document.createElement('div');
+    createElement.classList.add('bg-red-100');
+    createElement.classList.add('p-4');
+    createElement.classList.add('text-center');
+    createElement.classList.add('mb-5');
+    createElement.classList.add('rounded');
+    createElement.innerHTML = `
+<h3>Donate : ${inputFieldValue}</h3>
+<h3>Available Balance Now :${availableBalanceDecrease}</h3>
+<p>Date ${new Date()}</p>
+`
+    historyPage.appendChild(createElement);
+
+
+
     document.getElementById('avalable-balance').innerText=availableBalanceDecrease;
     const sum=inputFieldValue+textInner;
     document.getElementById('total2-donate').innerText=sum;
 });
+
+
+
